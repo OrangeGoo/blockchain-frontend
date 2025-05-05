f3e9333945cb7462c920ecb0324a23ae80190b81
OrangeGoo <gjc20021013@gmail.com>
2025-05-05 11:54:45 -0400

    fix: https

--- context/BlockchainContext.tsx
+++ context/BlockchainContext.tsx
@@ -89,10 +89,10 @@ export function BlockchainProvider({ children }: { children: ReactNode }) {
   const [nodeStatus, setNodeStatus] = useState(false);
   const [peers, setPeers] = useState<string[]>([]);
   // const [currentPort, setCurrentPort] = useState(5001);
-  const [nodeUrl, setNodeUrl] = useState("http://35.232.36.80:5001");
+  const [nodeUrl, setNodeUrl] = useState("https://35.232.36.80:5001");
 
   const baseURL = nodeUrl; // Base URL for the node
-  const trackerURL = "http://34.56.151.86:6000"; // Tracker URL
+  const trackerURL = "https://34.56.151.86:6000"; // Tracker URL
 
   useEffect(() => {
     const checkConnection = async () => {

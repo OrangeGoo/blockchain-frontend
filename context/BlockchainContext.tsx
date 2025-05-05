5e8e084d01ececef092b0d2f8942e6c288ec8639
OrangeGoo <gjc20021013@gmail.com>
2025-05-05 11:33:47 -0400

    fix: cors

--- context/BlockchainContext.tsx
+++ context/BlockchainContext.tsx
@@ -89,10 +89,10 @@ export function BlockchainProvider({ children }: { children: ReactNode }) {
   const [nodeStatus, setNodeStatus] = useState(false);
   const [peers, setPeers] = useState<string[]>([]);
   // const [currentPort, setCurrentPort] = useState(5001);
-  const [nodeUrl, setNodeUrl] = useState("http://localhost:5001");
+  const [nodeUrl, setNodeUrl] = useState("35.232.36.80:5001");
 
   const baseURL = nodeUrl; // Base URL for the node
-  const trackerURL = `http://localhost:6000`; // Tracker URL
+  const trackerURL = `34.56.151.86:6000`; // Tracker URL
 
   useEffect(() => {
     const checkConnection = async () => {
--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:host/:port/:path*",
-        destination: "http://:host::port/:path*",
-      },
-      {
-        source: "/tracker/:path*",
-        destination: "http://localhost:6000/:path*",
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
